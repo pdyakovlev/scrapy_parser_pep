a6626f4b9ca8ebd27d0d08a3721adb3f9ebdf5a6
PDY <pdyakovlev@gmail.com>
2023-11-05 20:36:55 +0500

    First commit.

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,37 @@
+import csv
+import datetime as dt
+from collections import defaultdict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class PepParsePipeline:
+    pep_status_count = defaultdict(int)
+    total_pep_count = 0
+
+    def open_spider(self, spider):
+        result_dir = BASE_DIR / 'results'
+        result_dir.mkdir(exist_ok=True)
+
+    def process_item(self, item, spider):
+        if item.get('status'):
+            self.total_pep_count += 1
+            self.pep_status_count[
+                item['status']] = self.pep_status_count.get(
+                item['status'], 0) + 1
+
+        return item
+
+    def close_spider(self, spider):
+        results = [('Cтатус', 'Количество')]
+        results.extend(self.pep_status_count.items())
+        results.append(('Total: ', self.total_pep_count))
+
+        file_format = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        file_name = f'status_summary_{file_format}.csv'
+        file_path = BASE_DIR / 'results' / file_name
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(results)
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,28 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        table_bodies = response.css('tbody')
+
+        for tr in table_bodies.css('tr'):
+            link_pep_page = tr.css('td').css('a::attr(href)').get()
+            if link_pep_page is not None:
+                yield response.follow(link_pep_page, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get()
+
+        data = {
+            'number': [int(s) for s in title.split() if s.isdigit()][0],
+            'name': title,
+            'status': response.css('dt:contains("Status") + dd::text').get()
+        }
+        yield PepParseItem(data)
